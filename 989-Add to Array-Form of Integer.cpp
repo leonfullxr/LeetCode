988a62c504b751192488135b6e9569f26978c8f9
Leon <87938446+leonfullxr@users.noreply.github.com>
2023-02-15 13:44:38 +0100

    Update to the final solution

--- 989-Add to Array-Form of Integer.cpp
+++ 989-Add to Array-Form of Integer.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        vector<int> output;
-        int number = 0;
-        int count = 1;
-
-        for(int i = num.size()-1; i >= 0; i--){
-            number += count * num[i];
-            count *= 10;
+        for(int i=num.size()-1;i>=0;i--){
+            num[i] += k;
+            k = num[i]/10;
+            num[i] %= 10;
         }
-
-        number += k;
-        vector<int>::iterator it = output.begin();
-
-        do{
-            it = output.insert(it,number % 10);
-            number /= 10;
-        }while(number != 0);
-
-        return output;
+        while(k > 0){
+            num.insert(num.begin(), k%10);
+            k /= 10;
+        }
+        return num;
     }
 };
